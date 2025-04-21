5729bb4a0c5f2420cfc4290219a7a3ba9989c076
Carlos Murillo <cmurillperez@gmail.com>
2025-04-21 08:35:46 +0200

    Primer commit
    Proyecto desde cero vacío, con todas las dependencias instaladas.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl">Hola mundo</h1>
+      <Button>Click aqui</Button>
+    </>
+  )
+}
+
+export default App
